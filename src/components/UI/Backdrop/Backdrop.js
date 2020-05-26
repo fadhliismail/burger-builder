8e38e75c76307e24a7b730d6cbb14b12e4c5c186
fadhliismail <mfadhliismail@gmail.com>
2020-05-26 11:56:44 +0100

    reuse backdrop

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -1,10 +1,16 @@
 import React from "react";
 import classes from "./Backdrop.module.css";
 
-const backdrop = (props) => (
-  <div>
-    props.show ? <div className={classes.Backdrop}></div> : null
-  </div>
-);
+const backdrop = (props) => {
+    console.log(props.show);
+
+    return (
+        <div>
+            {props.show ? (
+                <div className={classes.Backdrop} onClick={props.clicked}></div>
+            ) : null}
+        </div>
+    );
+};
 
 export default backdrop;
